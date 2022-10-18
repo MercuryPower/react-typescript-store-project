4fe8c225fdd0a809b690cc65a7f6eea7266839cc
MercuryPower <nikita.poker2015@yandex.ru>
2022-10-18 18:39:12 +0400

    addingRemoveIcon

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -4,6 +4,7 @@ import { IProduct } from "../models";
 import { CartPage } from "../pages/CartPage";
 import plus from "../images/+.svg"
 import minus from "../images/minus.svg"
+import removeBucket from "../images/removeBucket.svg"
 
 interface CartItemProps {
     id: number
@@ -19,13 +20,9 @@ export function CartItem({id,quantity} : CartItemProps, {product} :ProductProps 
      const item = headphones.find(i => i.id === id)
      if (item == null) return null
      
-     function plusCart(){
-        return quantity+1;
-     }
-
      return(
         <div>
-            <div className="flex shadow-2xl w-[633px]  ml-6  mt-2 border-2 rounded-3xl my-6">
+            <div className="flex justify-between shadow-2xl w-[633px]  ml-6  mt-2 border-2 rounded-3xl my-6">
                 <div className="px-4 py-2">
                     <img src={item.img} alt="" />
                         <div className="flex items-center justify-between px-4 py-2">
@@ -34,20 +31,17 @@ export function CartItem({id,quantity} : CartItemProps, {product} :ProductProps 
                             <button onClick={() => decreaseCartQuntity(id)}><img src={minus} className='pr-4' alt="" /></button>
                         </div>
                 </div>
-            <div className="flex flex-col justify-center">
-                <p>{item.title}</p>
-                <p>{item.price * quantity}₽</p>
+            <div className="flex flex-col justify-center mr-20">
+                <p className="font-semibold">{item.title}</p>
+                <p className=" text-gray-500 font-semibold tracking-widest">{item.price * quantity}₽</p>            
             </div>
-            </div>
-                {/* <div className="flex flex-col border-2 shadow-2xl rounded-3xl w-[348.98px] h-[114px] mx-20 my-14">
-                    <div className="flex justify-between py-2 px-4 shadow-2xl  ">
-                        <p className="px-4 py-2 font-medium text-lg">ИТОГО</p>
-                        <p className="px-4 py-2 font-medium text-lg"> ₽</p>
+                <div className="flex items-end py-2 pl-12">
+                    <p className=" font-semibold tracking-widest">{item.price * quantity}₽</p>
                 </div>
-            <div className="shadow-2xl">
-                <button className="bg-black w-full h-20 text-lg text-white rounded-3xl">Перейти к оформлению</button>
+            <div className="flex items-start">
+            <button  className="py-2 px-4" type="submit" onClick={() => removeFromCart(id)}><img src={removeBucket} alt="removeBucket" /></button>
             </div>
-        </div> */}
-        </div>
+            </div>
+            </div>
      )
 }
